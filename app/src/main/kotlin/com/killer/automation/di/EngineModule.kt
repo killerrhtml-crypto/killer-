ccb29eaad5a8e7ff4d362ba99891c85fa35cf822
edwinnes <edwinnunezsoriano@gmail.com>
2026-07-01 22:35:49 -0400

    feat(di): add EngineModule to provide DecisionEngine singleton via Hilt

---
+++ app/src/main/kotlin/com/killer/automation/di/EngineModule.kt
@@ -0,0 +1,26 @@
+package com.killer.automation.di
+
+import com.killer.automation.data.EncryptedDataManager
+import com.killer.automation.engine.DecisionEngine
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+/**
+ * Hilt module that provides the DecisionEngine singleton.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object EngineModule {
+
+    @Provides
+    @Singleton
+    fun provideDecisionEngine(encryptedDataManager: EncryptedDataManager): DecisionEngine {
+        // If DecisionEngine has an @Inject constructor accepting EncryptedDataManager,
+        // Hilt could construct it automatically. We provide it explicitly here to ensure
+        // the dependency is satisfied and to keep a single optimized instance.
+        return DecisionEngine(encryptedDataManager)
+    }
+}
